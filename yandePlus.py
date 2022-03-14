96394898f0233971ddcd71b1a1673545e8efcf9d
SnowFey <a0981120991@gmail.com>
2022-03-14 20:58:22 +0800

    Update yandePlus.py

--- yandePlus.py
+++ yandePlus.py
@@ -7,20 +7,9 @@ import os
 import asyncio
 
 
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-    "Accept-Encoding": "gzip, deflate, br", 
-    "Accept-Language": "zh-TW,zh;q=0.9", 
-    "Host": "example.com",  #目標網站 
-    "Sec-Fetch-Dest": "document", 
-    "Sec-Fetch-Mode": "navigate", 
-    "Sec-Fetch-Site": "none", 
-    "Upgrade-Insecure-Requests": "1", 
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36" #使用者代理
-}
-session_requests = requests.session()
-
-CharacterName = input("CharacterName:") #uruha_rushia
+tags = input("tags:") #https://yande.re/post?page=(i)&tags=(Tags)
+CharacterName = tags.replace("%28","").replace("%29","")
+print(CharacterName)
 print("first page:1")
 last1 = input("last page:")
 print("Going")
@@ -30,12 +19,11 @@ async def directlink_largeimg(url):
     response = await loop.run_in_executor(None, requests.get, url)
     soup = BeautifulSoup(response.text, "lxml")
 
-    results = soup.find_all("a", {"class": ["directlink largeimg","directlink smallimg"]})  #directlink largeimg "directlink medicineimg",
+    results = soup.find_all("a", {"class": ["directlink largeimg","directlink smallimg"]}) 
     thumbs = soup.find_all("a", {"class": "thumb"})
 
     image_links = [result.get("href") for result in results]  # 取得圖片來源連結
-    imgID_hrefs = [thumb.get("href") for thumb in thumbs] #https://yande.re/post/show/ID
-    #str(imgID_hrefs).replace("/post/show/","uruha_rushia_") 
+    imgID_hrefs = [thumb.get("href") for thumb in thumbs] #/post/show/ID
     await download(image_links, imgID_hrefs)
 
 
@@ -58,7 +46,7 @@ async def download(image_links, imgID_hrefs):
 def main():
     tasks = []
     for i in range(1, int(last1) + 1):
-        url = "https://yande.re/post?page=" + str(i) + "&tags=" + str(CharacterName)   #url=https://yande.re/post?page=1&tags=uruha_rushia
+        url = "https://yande.re/post?page=" + str(i) + "&tags=" + str(tags)  #url=https://yande.re/post?page=1&tags=uruha_rushia
         tasks.append(loop.create_task(directlink_largeimg(url)))
     loop.run_until_complete(asyncio.wait(tasks))
     print("Downloaded Finish")
