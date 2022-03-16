1ef5f23b231b7126dc9583b2f4d669899d41ff97
SnowFey <a0981120991@gmail.com>
2022-03-17 01:20:19 +0800

    Update yande_aiohttp.py

--- yande_aiohttp.py
+++ yande_aiohttp.py
@@ -57,4 +57,4 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
-    print("Downloaded Finish")
+    print("%s Downloaded Finish"%CharacterName)
