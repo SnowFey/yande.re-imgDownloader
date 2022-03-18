acaf932da0fb2a331230bbe30b957705a314efa3
樓梯妹妹 <a0919329@gmail.com>
2022-03-18 20:20:14 +0800

    aiohttp的建議
    我這幾天把整個架構用自己的方法改過來，抓圖的方法是可行的，但是目前還有很多問題要解決：
    - 請求過多，你可以想個方法去減少dt的時間
    - 請求的圖源可能有問題，這支程式雖然可以抓圖但抓的圖不會是對的

--- yande_aiohttp.py
+++ yande_aiohttp.py
@@ -1,60 +1,52 @@
-from aiohttp import ClientSession
+import os, asyncio
+
+import requests, aiohttp
+
 from bs4 import BeautifulSoup
-from lxml import html
-import asyncio, os, requests
 
-#          https://yande.re/post?page=(i)&tags=(Tags)
-# Example: https://yande.re/post?page=1&tags=Mochizuki_anna
-
-tags = input("tags: ")
-
-CharacterName = tags.replace("%28","").replace("%29","")
-print(f"fileName:{CharacterName}")
-print("first page: 1")
-
-last1 = int(input("last page: "))
-
-print("Going")
-
-if not os.path.exists(str(CharacterName)):
-    os.mkdir(str(CharacterName))  # 建立資料夾
+async def download(link:str, imgID:list, tags, session):
+    async with session.get(link) as response:
+        with open(f"./{tags}/{tags}{imgID}.jpg", mode="wb") as file:
+            file.write(await response.read())
 
 async def main():
-    weblinks = [] 
-    for page in range(1, (last1+1)):
-        weblinks.append(f"https://yande.re/post?page={page}&tags={tags}")
+    tags = input("tags: ")
+    last_page = int(input(f"Starts from page 1, \nEnter last page: "))
 
-    async with ClientSession() as session:
-        tasks = [asyncio.create_task(directlink_largeimg(weblink, session)) for weblink in weblinks]
-        await asyncio.gather(*tasks)
+    print("Strat fetching")
 
-async def directlink_largeimg(weblink, session):
-    async with session.get(weblink) as response:  #非同步發送請求
-        html_text = await response.text()
-        soup = BeautifulSoup(html_text, "lxml")  # 解析HTML原始碼
- 
+    CharacterName = tags.replace("%28","").replace("%29","")
+
+    if not os.path.exists(CharacterName):
+        os.mkdir(CharacterName)  # 建立資料夾
+
+    for i in range(1, last_page+1):
+        url = f"https://yande.re/post?page={i}tags={tags}"
+        print(f"requesting to: {url}")
+
+
+        # I will find a solution to replace this garbage down here
+        response = await loop.run_in_executor(None, requests.get, url)
+
+        
+        soup = BeautifulSoup(response.text, "lxml")
         results = soup.find_all("a", {"class": ["directlink largeimg","directlink smallimg"]}) 
-        thumbs = soup.find_all("a", {"class": "thumb"})
-
+        thumbs = soup.find_all("a", {"class": "thumb"}) 
         image_links = [result.get("href") for result in results]  # 取得圖片來源連結
         imgID_hrefs = [thumb.get("href") for thumb in thumbs] #/post/show/ID
-        
-        await download(image_links, imgID_hrefs)
 
-async def download(image_links, imgID_hrefs):
-    x = 0
-    for index, link in enumerate(image_links):
-
-        img = requests.get(link)  # 下載圖片 
-
-        with open("./%s\\"%CharacterName + imgID_hrefs[x].replace("/post/show/","%s_"%CharacterName)  + ".jpg", "wb") as file:  # 開啟資料夾及命名圖片檔
-            file.write(img.content)  # 寫入圖片的二進位碼
-
-            print(imgID_hrefs[x].replace("/post/show/",""))
-            x += 1
+        tasks = []
+        async with aiohttp.ClientSession() as session:
+            session = session
+            for _, link in enumerate(image_links):
+                for imgID in imgID_hrefs:
+                    imgID = imgID[11:]
+                    tasks.append(loop.create_task(download(link, imgID ,tags, session)))
+            await asyncio.gather(*tasks)
+    session.close()
+    print("Downloaded Finished")
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
-    print("%s Downloaded Finish"%CharacterName)
